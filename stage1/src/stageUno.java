6f5cdbf2682f579c57cd09fd71264d84efb3d0ee
BenjaminSum <88011394+BenjaminSum@users.noreply.github.com>
2022-04-04 14:56:46 +1000

    Assignment Code updated from multiple files into one

--- stage1/src/stageUno.java
+++ stage1/src/stageUno.java
@@ -1,104 +1,164 @@
 import java.io.*;
-import java.net.Socket;
-import java.util.ArrayList;
+import java.net.*;
 
 public class stageUno {
-    public static void main(String args[]) throws IOException {
-        // set up connection
-        Socket s = new Socket("localhost", 50000);
-        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("127.0.0.1", 50000);
+            BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 
-        String response = ""; //store incoming messages from server
-        String[] responseArray = {}; // store split server response as components
-        ArrayList<Server> allServers = new ArrayList<Server>(); //store current available servers
-        ArrayList<Server> largestServers = new ArrayList<Server>(); //store only largest servers
-        Job currentJob = new Job(); //store current job
-        Server chosenServer = new Server(); //store server selected for current job
+            String sendSTR = "", recieveSTR = ""; // Initialises the String objects used to hold sending and recieved
+                                                  // messages
 
-        // hand-shaking 
-        response = send("HELO", dout, br);
-        response = send("AUTH " + System.getProperty("user.name"), dout, br);
-        response = send("REDY", dout, br);
+            handshake(sendSTR, recieveSTR, din, dout); // Handshake
 
-        // main loop - schedule all jobs
-        while (true) {
-            if(response.contains("NONE")){ // no more jobs - quit
-                break;
-            }
+            // Initialising variables
+            String lType = ""; // Largest Server Name
+            int lCore = 0; // Largest Server Core size
+            int sCount = 0; // Count of largest server
+            boolean checked = false; // Checks if the client has already requested server information
 
-            // recieved job - store job and get capable servers
-            else if (response.contains("JOBN") || response.contains("JOBP")) {
-                responseArray = response.split("\s");
-                currentJob = new Job(responseArray[1], responseArray[2], responseArray[3], responseArray[4],
-                        responseArray[5], responseArray[6]);
-                
-                // get all server data initially
-                if(allServers.isEmpty()){ 
-                    response = send("GETS All", dout, br);
-                    response = send("OK", dout, br); //to recieve all server data
-                    response = send("OK", dout, br); //to recieve '.' at end
-                
-                    // inner loop - store all server data
-                    while(true){
-                        String[] server = response.split("\s");
-                        if(server.length>1){ // ensure no more data message '.' is not attempted to be added
-                            allServers.add(new Server(server[0], server[1], server[3], server[4], server[5], server[6]));
+            int job = 0; // jobID always start at 0
+            int serverID = 0; // ServerID always start from 0
+
+            while (!recieveSTR.equals("NONE")) {
+                // Requesting job information
+                sendSTR = "REDY\n";
+                dout.write((sendSTR).getBytes());
+                dout.flush();
+
+                recieveSTR = din.readLine();
+                System.out.println("Server says:" + recieveSTR);
+
+                String[] jobCheck = recieveSTR.split(" ");
+
+                if (checked == false) {
+                    // Request server information
+                    sendSTR = "GETS All\n";
+                    dout.write((sendSTR).getBytes());
+                    dout.flush();
+
+                    recieveSTR = din.readLine();
+                    System.out.println("Server says:" + recieveSTR);
+
+                    // Finding amount of servers
+                    String[] dataArr = recieveSTR.split(" ");
+                    int nRecs = Integer.parseInt(dataArr[1]);
+
+                    okACK(sendSTR, recieveSTR, din, dout);
+
+                    // Cataloging Server sizes
+                    for (int i = 0; i < nRecs; ++i) {
+                        recieveSTR = din.readLine();
+                        String[] serverINFO = recieveSTR.split(" ");
+
+                        if (lCore < Integer.parseInt(serverINFO[4])) {
+                            lCore = Integer.parseInt(serverINFO[4]);
+                            lType = serverINFO[0];
                         }
-                        response = br.readLine();
-                        if(response.contains(".")){
-                            break;
+
+                        if (lType.equals(serverINFO[0])) {
+                            sCount = 1 + Integer.parseInt(serverINFO[1]);
                         }
+
+                        System.out.println("Server says:" + recieveSTR);
                     }
+                    System.out.println("Largest Server Type: " + lType + " Cores: " + lCore + " Count " + sCount);
 
-                    // get all largest servers for LRR
-                    for (Server server : allServers) {
-                        if(largestServers.isEmpty()){
-                            largestServers.add(server);
-                        }
-                        else if(server.cores > largestServers.get(0).cores){
-                            largestServers.clear();
-                            largestServers.add(server);
-                        }
-                        else if(server.cores == largestServers.get(0).cores){
-                            largestServers.add(server);
-                        }
-                    }
+                    okACK(sendSTR, recieveSTR, din, dout);
+
+                    recieveSTR = din.readLine();
+                    System.out.println("Server says:" + recieveSTR);
+
+                    checked = true;
+
                 }
 
-                // choose server for current job 
-                chosenServer = largestRoundRobin(currentJob, largestServers);
+                // Reading job message and schedule
+                if (jobCheck[0].equals("JOBN")) {
+                    // schedule job
+                    schdAction(sendSTR, recieveSTR, din, dout, job, lType, serverID);
 
-                //schedule current job and get next job
-                response = send("SCHD "+currentJob.id+" "+chosenServer.type+" "+chosenServer.id, dout, br);
-                response = send("REDY", dout, br);
+                    job++; // increments job and server to next appropriate job/server
+                    serverID++;
+                    if (serverID >= sCount) {
+                        serverID = 0;
+                    }
+                }
             }
-
-            //recieved completed job or server status info - client still ready
-            else if(response.contains("JCPL") || response.contains("RESF") || response.contains("RESR")){
-                response = send("REDY", dout, br);
-            }
-            else break; // recieved unhandled message from server
+            // Quitting and disconnecting from server
+            quitAction(sendSTR, recieveSTR, din, dout, s); // quits and closes socket
+        } catch (Exception e) {
+            System.out.println(e);
         }
-
-        // quit server and close connection
-        response = send("QUIT", dout, br);
-        dout.close();
-        s.close();
-
     }
 
-    public static String send(String message, DataOutputStream dout, BufferedReader br) throws IOException{
-        // handles all sending messages to server and recieveing responces
-        dout.write((message + "\n").getBytes());
-        String response = br.readLine();
-        System.out.println("C: "+message);
-        System.out.println("S: "+response);
-        return response;
+    // completes handshake to connect with server
+    static void handshake(String send, String recieve, BufferedReader din, DataOutputStream dout) {
+        try {
+            send = "HELO\n";
+            dout.write((send).getBytes());
+            dout.flush();
+
+            recieve = din.readLine();
+            System.out.println("Server says:" + recieve);
+
+            send = "AUTH benjamin\n";
+            dout.write((send).getBytes());
+            dout.flush();
+
+            recieve = din.readLine();
+            System.out.println("Server says:" + recieve);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
-    public static Server largestRoundRobin(Job job, ArrayList<Server> servers){
-        // implements 'Largest Round Robin' by scheduling each job on the modulo of its id and the ammount of servers
-        return servers.get(job.id%servers.size());
+    // sends a quit message and closes socket
+    static void quitAction(String send, String recieve, BufferedReader din, DataOutputStream dout, Socket s) {
+        try {
+            send = "QUIT\n";
+            dout.write((send).getBytes());
+            dout.flush();
+
+            recieve = din.readLine();
+            System.out.println("Server says:" + recieve);
+
+            dout.close();
+            s.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    // sends a scheduling message
+    static void schdAction(String send, String recieve, BufferedReader din, DataOutputStream dout, int job,
+            String lType, int serverID) {
+        try {
+            send = "SCHD " + job + " " + lType + " " + serverID + "\n";
+            dout.write((send).getBytes());
+            dout.flush();
+
+            recieve = din.readLine();
+            System.out.println("Server says:" + recieve);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    // Sends an OK acknowledment
+    static void okACK(String send, String recieve, BufferedReader din, DataOutputStream dout) {
+        try {
+            send = "OK\n";
+            dout.write((send).getBytes());
+            dout.flush();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
